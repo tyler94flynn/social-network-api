2be3c455ca1e5b9e491a7b26ca11874428276090
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-29 13:49:31 -0400

    server & routes index backbone

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+//connect mongoose here
+
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
