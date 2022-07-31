599dfeac1b29b7926cedc4da425f9fa3662501b0
Tyler Flynn <tyler94flynn@gmail.com>
2022-07-31 16:30:12 -0400

    fixed thought model

--- models/Thought.js
+++ models/Thought.js
@@ -28,4 +28,10 @@ const thoughtSchema = new Schema({
     id: false
 });
 
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
 module.exports = Thought;
